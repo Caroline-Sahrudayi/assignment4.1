15ac332cdbdc1004f0ae3d64d158e276e470a838
Caroline-Sahrudayi <carolineps777@gmail.com>
2018-07-05 00:20:00 +0530

    just minor changes

--- app/src/main/java/com/jkmca/caroline/loginpage/MainActivity.java
+++ app/src/main/java/com/jkmca/caroline/loginpage/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String emailstr = emailet.getText().toString();
                 String pwstr = pwet.getText().toString();
-                if (emailstr.equals("caroline") && pwstr.equals("eke7777")) {
+                if (emailstr.equals("carolineps777@gmail.com") && pwstr.equals("eke7777")) {
                     Toast.makeText(MainActivity.this, "login success", Toast.LENGTH_SHORT).show();
                 } else {
                     toast=Toast.makeText(MainActivity.this, "Wrong ID / Pwd Combination", Toast.LENGTH_SHORT);
